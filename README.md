1f3b16af154c0ce1f3211a4c4c7d9e2afa8e79be
Yu Harukaze <yukaze@outlook.com>
2021-07-04 16:52:58 -0400

    added description to README.md

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # font_face
+To use in your project, put 'bitmap', 'util', and 'font_face' hpp and cpp files in a folder together and include 'font_face.hpp' where needed.
+The texture atlas class is not required and was only used to help with examining the results of the glyph rasterizer.
+
+Getting a glyph bitmap looks like this:
+    tou::font_face face("arial.ttf");
+    uint16_t ch = 0x0042; // letter 'B'
+    float pointsize = 64.0f;
+    auto glyph = face.get_glyph_bitmap(ch, pointsize, true, true);
+    makefile("glyph.bmp", glyph.image);
+
+makefile can be defined as:
+    void makefile(const std::string& filename, const tou::bitmap_image& bmp)
+    {
+	    std::vector<char> v;
+	    bmp.file(v);
+	    std::ofstream out;
+	    out.open(filename, std::ios::binary | std::ios::out);
+	    out.write(v.data(), v.size());
+	    out.close();
+    }
+
+Known Issues
+--------------
+In the rasterizer function, some small regions of a glyph may end up unfilled. This happens because the fill operation expects the calculated outline to be 'nice' (no isolated jumps in a segment of an outline) but an outline may not always be 'nice' especially at point sizes less than 16.
+
+Misc
+--------------
+- Contributions are welcome
+- You may use the code however you please. If you do, a link to my github page would be appreciated but is not required.
+
+Special Thanks
+--------------
+- https://docs.microsoft.com/en-us/typography/opentype/spec/
+- https://opentype.js.org/
+- https://stuff.mit.edu/afs/athena/astaff/source/src-9.0/third/freetype/docs/glyphs/glyphs-6.html
--- src/main_sandbox.cpp
+++ src/main_sandbox.cpp
@@ -27,43 +27,43 @@ int main(int argc, char** argv)
 	if (!face)
 		return -1;
 
-	//uint16_t ch = 0x30E2;
-	//std::string filename  = "glyph" + std::to_string(ch) + "_@" + std::to_string(static_cast<int>(pointsize)) + "pt_" + std::to_string(fileiteration) + "full.bmp";
-	//std::string filenameo = "glyph" + std::to_string(ch) + "_@" + std::to_string(static_cast<int>(pointsize)) + "pt_" + std::to_string(fileiteration) + "outline.bmp";
-	//auto glyph = face.get_glyph_bitmap(ch, pointsize, true, true);
-	//auto glypho = face.get_glyph_bitmap(ch, pointsize, true, false);
-	//makefile(filename, glyph.image);
-	//makefile(filenameo, glypho.image);
+	uint16_t ch = 0x30E2;
+	std::string filename  = "glyph" + std::to_string(ch) + "_@" + std::to_string(static_cast<int>(pointsize)) + "pt_" + std::to_string(fileiteration) + "full.bmp";
+	std::string filenameo = "glyph" + std::to_string(ch) + "_@" + std::to_string(static_cast<int>(pointsize)) + "pt_" + std::to_string(fileiteration) + "outline.bmp";
+	auto glyph = face.get_glyph_bitmap(ch, pointsize, true, true);
+	auto glypho = face.get_glyph_bitmap(ch, pointsize, true, false);
+	makefile(filename, glyph.image);
+	makefile(filenameo, glypho.image);
 
-	std::string atlasname = "atlas" + std::to_string(fileiteration) + ".bmp";
-	tou::texture_atlas<uint16_t> atlas(1024, 1024, { 0xFF, 0xFF, 0xFF, 0xFF });
-	for (uint16_t ch = 0x0021; ch <= 0x007E; ch++)
-	{
-		auto glyph = face.get_glyph_bitmap(ch, pointsize, true, true);
-		if (glyph.id != 0)
-			atlas.push(glyph.image, ch);
-	}
-	pointsize = 12.0f;
-	for (uint16_t ch = 0x30A0; ch <= 0x30FF; ch++)
-	{
-		auto glyph = face.get_glyph_bitmap(ch, pointsize, true, true);
-		if (glyph.id != 0)
-			atlas.push(glyph.image, ch);
-	}
-	for (uint16_t ch = 0x3040; ch <= 0x309F; ch++)
-	{
-		auto glyph = face.get_glyph_bitmap(ch, pointsize, true, true);
-		if (glyph.id != 0)
-			atlas.push(glyph.image, ch);
-	}
-	for (uint16_t ch = 0x4E00; ch <= 0x4F00; ch++)
-	{
-		auto glyph = face.get_glyph_bitmap(ch, pointsize, true, true);
-		if (glyph.id != 0)
-			if (!atlas.push(glyph.image, ch))
-				break;
-	}
-	makefile(atlasname, atlas.get());
+	//std::string atlasname = "atlas" + std::to_string(fileiteration) + ".bmp";
+	//tou::texture_atlas<uint16_t> atlas(1024, 1024, { 0xFF, 0xFF, 0xFF, 0xFF });
+	//for (uint16_t ch = 0x0021; ch <= 0x007E; ch++)
+	//{
+	//	auto glyph = face.get_glyph_bitmap(ch, pointsize, true, true);
+	//	if (glyph.id != 0)
+	//		atlas.push(glyph.image, ch);
+	//}
+	//pointsize = 12.0f;
+	//for (uint16_t ch = 0x30A0; ch <= 0x30FF; ch++)
+	//{
+	//	auto glyph = face.get_glyph_bitmap(ch, pointsize, true, true);
+	//	if (glyph.id != 0)
+	//		atlas.push(glyph.image, ch);
+	//}
+	//for (uint16_t ch = 0x3040; ch <= 0x309F; ch++)
+	//{
+	//	auto glyph = face.get_glyph_bitmap(ch, pointsize, true, true);
+	//	if (glyph.id != 0)
+	//		atlas.push(glyph.image, ch);
+	//}
+	//for (uint16_t ch = 0x4E00; ch <= 0x4F00; ch++)
+	//{
+	//	auto glyph = face.get_glyph_bitmap(ch, pointsize, true, true);
+	//	if (glyph.id != 0)
+	//		if (!atlas.push(glyph.image, ch))
+	//			break;
+	//}
+	//makefile(atlasname, atlas.get());
 
 	return 0;
 }
